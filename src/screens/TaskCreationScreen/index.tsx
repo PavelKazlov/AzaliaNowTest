962c8fe4425c4b84292ecbd0a55386419bc81ee3
PavelKazlov <6956717@gmail.com>
2023-01-06 17:30:21 +0300

    Created input

--- src/screens/TaskCreationScreen/index.tsx
+++ src/screens/TaskCreationScreen/index.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, {createRef, useState} from 'react'
 import {Text, View} from 'react-native'
 import {styles} from './styles'
+import {Input} from '@rneui/themed'
+import {Colors} from '@/constants'
+import {Button} from '@/components'
 
 export const TaskCreationScreen = () => {
+  const [inputValue, setInputValue] = useState('')
+  const input = createRef()
+
+  const handleChange = (value: string) => {
+    console.log('value => ', value)
+    setInputValue(value)
+  }
+
+  const addTaskHandler = () => {
+    input?.current?.blur()
+    input?.current?.clear()
+    setInputValue('')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Add Your Task Here. Coming soon...</Text>
+      <Input
+        ref={input}
+        value={inputValue}
+        onChangeText={val => setInputValue(val)}
+        placeholder="Текст новой задачи"
+        inputStyle={{padding: 12}}
+        containerStyle={{paddingHorizontal: 0}}
+        inputContainerStyle={{
+          borderWidth: 1,
+          borderColor: Colors.secondaryButtonBackground,
+          borderRadius: 8,
+          paddingLeft: 5,
+          height: 46,
+        }}
+      />
+      <Button disabled={inputValue === ''} onPress={addTaskHandler}>
+        Добавить
+      </Button>
+      <Text>{inputValue}</Text>
     </View>
   )
 }
